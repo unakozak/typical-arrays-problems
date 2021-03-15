bd5b9e22182774b2de18cafdb0f5378d05ba9f89
Uliana Kozak <una.kozak@mail.ru>
2021-03-15 21:42:32 +0300

    feat: solve typical-arrays-problems problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    else
+        return array.sort(function sub(a, b) {
+            return a - b;
+        })[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    else
+        return array.sort(function sub(a, b) {
+            return a - b;
+        })[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    else return array.reduce((acc, a) => acc + a) / array.length;
+};
